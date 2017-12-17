aaaf9ad59d125f2489acf2b3e9d2af2ef463e98b
toblu302 <toblu302@users.noreply.github.com>
2017-12-17 12:24:45 +0100

    Improved solution, using the fact that 0 is always at the front of the list

--- 2017/Day17/part2.py
+++ 2017/Day17/part2.py
@@ -2,21 +2,12 @@ import sys
 
 def get_answer(steps):
   current_position = 0
-  before_0 = 0
-  after_0 = 0
   current_after_0 = 0
   for x in range(1,50000001):
-    current_position = (current_position + steps)%(before_0+1+after_0)
-    #print(current_position)
-    if current_position == before_0:
-      #inserting something right after 0
+    current_position = (current_position + steps)%(x)
+    if current_position == 0:
       current_after_0 = x
-    if current_position < before_0:
-      before_0 += 1
-    else:
-      after_0 += 1
-    #print(current_after_0)
-    current_position = (current_position + 1)%(before_0+1+after_0)
+    current_position = (current_position + 1)%(x+1)
 
   return current_after_0
 
